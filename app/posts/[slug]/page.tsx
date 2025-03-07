0e12a1eb66f4ee78d2f6e6026c56365a843d9526
Natalia <mientuska00@gmail.com>
2025-03-07 22:39:57 +0100

    fix

--- app/posts/[slug]/page.tsx
+++ app/posts/[slug]/page.tsx
@@ -2,22 +2,25 @@ import prisma from '@/lib/db';
 
 export default async function PostsSingularPage({
   params,
-} : {
-  params: {
-    slug: string;
-  };
+}: {
+  params: { slug: string };
 }) {
+  if (!params?.slug) {
+    return <p>Post not found</p>;
+  }
 
   const post = await prisma.post.findUnique({
-    where: {
-      slug: params.slug,
-    },
+    where: { slug: params.slug },
   });
-  
+
+  if (!post) {
+    return <p>Post not found</p>;
+  }
+
   return (
-    <main className='flex flex-col items-center justify-center min-h-screen py-2'>
-      <h1>{post?.title}</h1>
-      <p>{post?.content}</p>
+    <main className="flex flex-col items-center justify-center min-h-screen py-2">
+      <h1>{post.title}</h1>
+      <p>{post.content}</p>
     </main>
   );
 }
