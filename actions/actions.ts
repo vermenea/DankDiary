9d0adc71883dd81d53e979056fd5733e109b9dc0
Natalia <mientuska00@gmail.com>
2025-03-07 23:01:29 +0100

    deleting image from model

--- actions/actions.ts
+++ actions/actions.ts
@@ -9,7 +9,6 @@ export async function createPost(formData: FormData) {
       title: formData.get('title') as string,
       slug: (formData.get('title') as string).toLowerCase().replace(/\s/g, '-'),
       content: formData.get('content') as string,
-      image: formData.get('image') as string,
     },
   });
 
